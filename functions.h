a0e5a0ad0726d9ae3e396c5f0175d6e914fbcca2
Angelo Williams <awilliams21@wooster.edu>
2018-11-18 17:12:10 -0500

    Add some documentation to functions.h

--- functions.h
+++ functions.h
@@ -6,10 +6,24 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* getDataFromFile()
+* Parameters:
+	FILE *infile: a pointer to the file containing the data set
+	float setosaData[][4]: a multidimensional array that will contain the Setosa data
+	float versicolorData[][4]: a multidimensional array that will contain the Versicolor data
+	float virginicaData[][4]: a multidimensional array that will contain the Virginica data
+* Return: void
+*/
 void getDataFromFile(FILE *infile, float setosaData[][4], float versicolorData[][4], float virginicaData[][4]);
-//Standard deviation function, num should be 149
+
+/*
+	Function under construction
+*/
 float calculateSD(float data[], int num);
 
+/*
+Function under construction
+*/
 float mean(float data[], int num);
 
 #endif
