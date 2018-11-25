b15622f9386eb9065cb94c174e569dbfa14b8d46
Angelo Williams <awilliams21@wooster.edu>
2018-11-25 14:50:58 -0500

    Add declaration for new function PtoX and updated declarations for other functions.

--- functions.h
+++ functions.h
@@ -1,3 +1,5 @@
+// Author: Angelo Williams, Oliver Hamburger, Tom Kang
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -19,23 +21,29 @@
 void getDataFromFile(FILE *infile, float setosaData[][4], float versicolorData[][4], float virginicaData[][4]);
 
 /*
-	Function under construction
+* Parameters:
+	float data[]: the array of floats to find the standard deviation of
+	int num: the amount of floats in data[]
+* Return: float, the standard deviation of data[]
 */
-float calculateSD(float data[], int num, int dataPoint);
+float calculateSD(float data[], int num);
 
 /*
-Function under construction
+* Parameters:
+	float data[]: the array of floats to find the mean of
+	int num: the amount of floats in data[]
+* Return: float, the mean of data[]
 */
-float mean(float data[], int num, int dataPoint);
+float mean(float data[], int num);
 
 /*
-Function under construction
+* Parameters:
+	float data[][4]: a multidimensional array containing the data for a class
+	int sizeData: the amount of samples for the class being used
+	int indexOfDataP: the index of the data point being tested
+	int attributeIndex: the current attribute being tested
+* Return: float, PtoX
 */
-float sum(float data[], int num);
-
-/*
-Function under construction
-*/
-float PtoX(float data[], int length, int indexOfdataP);
+float PtoX(float data[][4], int sizeData, int indexOfDataP, int attributeIndex);
 
 #endif
