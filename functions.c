3d57ccfebd48bae1fe4a385818cf377bf6361dfb
Angelo Williams <awilliams21@wooster.edu>
2018-11-26 22:47:01 -0500

    Finish and implement all functions for classifier. Classifier works with data, may need some testing

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "functions.h"
 
-// Author: Tom Kang
+// Author: Tom Kang, Oliver Hamburger
 float mean(float data[], int num) {
 	float sumData, mean;
 
@@ -29,18 +29,40 @@ float calculateSD(float data[], int num) {
 }
 
 
+// Author: Tom Kang, Angelo Williams
+int compare(float a, float b, float c) {
+	int result;
+
+	if (a > b && a > c)
+		result = 1;
+	else if (b > a && b > c)
+		result = 2;
+	else if (c > a && c > b)
+		result = 3;
+	else {
+		printf("\nERROR in compare() function:\na = %f\nb = %f\nc = %f\n\n", a, b, c);
+	}
+
+	return result;
+}
+
+
 // Author: Oliver Hamburger
-float PtoX(float data[][4], int sizeData, int indexOfDataP, int attributeIndex) {
+float PtoX(float dataToTestAgainst[][4], float dataToTakePointFrom[][4],
+				int sizeData, int indexOfDataP, int attributeIndex,
+				bool dataPointInTestSet) {
 	// Create temp array to hold all values for a certain dataset and 
 	//	 certain attribute except the value currently being tested
 	int sizeTemp = sizeData - 1;
 	float temp[sizeTemp];
-
+	
 	// Populate temp array
 	int tempCounter = 0;
 	for (int i = 0; i < sizeData; i++) {
-		if (i != indexOfDataP) {
-			temp[tempCounter] = data[i][attributeIndex];
+		if (dataPointInTestSet && i == indexOfDataP)
+			continue;
+		else {
+			temp[tempCounter] = dataToTestAgainst[i][attributeIndex];
 			++tempCounter;
 		}
 	}
@@ -52,14 +74,79 @@ float PtoX(float data[][4], int sizeData, int indexOfDataP, int attributeIndex) 
 	float SD = calculateSD(temp, sizeTemp);
 
 	// Calculate PofX
-	float SDsquared = ldexp(SD, 2);
-	float top = ldexp((data[indexOfDataP][attributeIndex] - dataMean), 2);
+	float SDsquared = pow(SD, 2);
+	float top = pow((dataToTakePointFrom[indexOfDataP][attributeIndex] - dataMean), 2);
 
-	first = 1 / (sqrt(2 * M_PI*(SDsquared)));
+	first = 1 / (sqrt(2 * M_PI * SDsquared));
 	second = exp(-top / (2 * SDsquared));
 	end = first * second;
 
 	return end;
+}
+
+
+// Author: Tom Kang
+float getClassProb(float dataToTestAgainst[][4], float dataToTakePointFrom[][4],
+						int sizeData, int indexOfDataP, bool dataPointInTestSet) {
+	float x1Prob, x2Prob, x3Prob, x4Prob;
+	float classProb;
+
+	// Calculate the probability that the data point has each attribute
+	x1Prob = PtoX(dataToTestAgainst, dataToTakePointFrom, sizeData, indexOfDataP, 0, dataPointInTestSet);
+	x2Prob = PtoX(dataToTestAgainst, dataToTakePointFrom, sizeData, indexOfDataP, 1, dataPointInTestSet);
+	x3Prob = PtoX(dataToTestAgainst, dataToTakePointFrom, sizeData, indexOfDataP, 2, dataPointInTestSet);
+	x4Prob = PtoX(dataToTestAgainst, dataToTakePointFrom, sizeData, indexOfDataP, 3, dataPointInTestSet);
+
+	// Calculate the probability that the data point is in the class being tested against
+	classProb = x1Prob * x2Prob * x3Prob * x4Prob;
+
+	return classProb;
+}
+
+
+// Author: Tom Kang, Angelo Williams
+int totalProb(float dataToTakePointFrom[][4], int indexOfDataP,
+				float setosaData[][4], float versicolorData[][4], 
+				float virginicaData[][4], int setosaCount, int versicolorCount,
+				int virginicaCount, int totalCount, int setFlag) {
+	float setosaProb, versicolorProb, virginicaProb;
+	int result;
+	
+	// Lower total count by one since the sample being tested is not being tested against
+	totalCount -= 1;
+	bool pointInSetosa, pointInVersicolor, pointInVirginica = false;
+
+	// Set the bool of the corresponding data set to true if the data point is in that set
+	// Lower the count for that set by one since a sample from that set is being tested
+	if (setFlag == 1) {
+		pointInSetosa = true;
+		setosaCount -= 1;
+	}
+	else if (setFlag == 2) {
+		pointInVersicolor = true;
+		versicolorCount -= 1;
+	}
+	else if (setFlag == 3) {
+		pointInVirginica = true;
+		virginicaCount -= 1;
+	}
+
+	// Calculate the probability that the data point is a setosa
+	setosaProb = getClassProb(setosaData, dataToTakePointFrom, setosaCount, indexOfDataP, pointInSetosa);
+	setosaProb *= ((float)setosaCount / (float)totalCount);
+
+	// Calculate the probability that the data point is a versicolor
+	versicolorProb = getClassProb(versicolorData, dataToTakePointFrom, versicolorCount, indexOfDataP, pointInVersicolor);
+	versicolorProb *= ((float)versicolorCount / (float)totalCount);
+
+	// Calculate the probability that the data point is a virginica
+	virginicaProb = getClassProb(virginicaData, dataToTakePointFrom, virginicaCount, indexOfDataP, pointInVirginica);
+	virginicaProb *= ((float)virginicaCount / (float)virginicaCount);
+
+	// Find the largest of these three probabilities
+	result = compare(setosaProb, versicolorProb, virginicaProb);
+
+	return result;
 }
 
 
@@ -78,6 +165,8 @@ void getDataFromFile(FILE *infile, float setosaData[][4], float versicolorData[]
 	int virginicaCount = 0;
 
 	int i = 0;
+	// Scan through file and add data points to their respective data set
+	// Increment the various counters as their data points are parsed
 	while (!feof(infile)) {
 		fscanf(infile, "%f", &x1);
 		fscanf(infile, "%f", &x2);
--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "functions.h"
 
-const _Bool DEBUG_MODE = 1;
-
 // Author: Oliver Hamburger, Tom Kang, Angelo Williams
 int main(){
 	FILE *infile;
@@ -14,6 +12,8 @@ int main(){
 	
 	float temp;
 	char name[50];
+
+	// Prepare the data file to be parsed
 	while (!feof(infile)) {
 		fscanf(infile, "%f", &temp);
 		fscanf(infile, "%f", &temp);
@@ -42,52 +42,52 @@ int main(){
 	float versicolorData[versicolorCount][4];
 	float virginicaData[virginicaCount][4];
 	
+	// Populate data sets with data points from data file
 	getDataFromFile(infile, setosaData, versicolorData, virginicaData);
 
-	if (DEBUG_MODE) {
-		for (int i = 0; i < 50; ++i) {
-			printf("%f %f %f %f %s\n", setosaData[i][0], setosaData[i][1], setosaData[i][2], setosaData[i][3], "Irissesota");
-		}
-		for (int i = 0; i < 50; ++i) {
-			printf("%f %f %f %f %s\n", versicolorData[i][0], versicolorData[i][1], versicolorData[i][2], versicolorData[i][3], "Irisversicolor");
-		}
-		for (int i = 0; i < 50; ++i) {
-			printf("%f %f %f %f %s\n", virginicaData[i][0], virginicaData[i][1], virginicaData[i][2], virginicaData[i][3], "Irisvirginica");
-		}
+	// correctCount will be incremented by 1 every time a sample is correctly predicted
+	int correctCount = 0;
+	int currentResult;
+	char resultMap[4][11] = { "", "Setosa", "Versicolor", "Virginica" };
 
-		int index = 0;
-		//for all the ptox values, loop through index 0-49
-		printf("%f\n", PtoX(setosaData, setosaCount, index, 0));
-		printf("%f\n", PtoX(setosaData, setosaCount, index, 1));
-		printf("%f\n", PtoX(setosaData, setosaCount, index, 2));
-		printf("%f\n", PtoX(setosaData, setosaCount, index, 3));
+	// Predict all Setosa samples
+	for (int i = 0; i < setosaCount; ++i) {
+		currentResult = totalProb(setosaData, i, setosaData,
+			versicolorData, virginicaData, setosaCount,
+			versicolorCount, virginicaCount, totalCount, 1);
+		
+		if (currentResult == 1)
+			++correctCount;
+
+		printf("Setosa #%i prediction: %s\n", i + 1, resultMap[currentResult]);
 	}
 
-	/*
-	use p(xi|y) equation for the following
+	// Predict all Versicolor samples
+	for (int i = 0; i < versicolorCount; ++i) {
+		currentResult = totalProb(versicolorData, i, setosaData,
+			versicolorData, virginicaData, setosaCount,
+			versicolorCount, virginicaCount, totalCount, 2);
+
+		if (currentResult == 2)
+			++correctCount;
+
+		printf("Versicolor #%i prediction: %s\n", i + 1, resultMap[currentResult]);
+	}
+
+	// Predict all Virginica samples
+	for (int i = 0; i < virginicaCount; ++i) {
+		currentResult = totalProb(virginicaData, i, setosaData,
+			versicolorData, virginicaData, setosaCount ,
+			versicolorCount, virginicaCount, totalCount, 3);
+
+		if (currentResult == 3)
+			++correctCount;
+
+		printf("Virginica #%i prediction: %s\n", i + 1, resultMap[currentResult]);
+	}
 	
-		x1Setosa[50];  p(x1|a)
-		x2Setosa[50];  p(x2|a)
-		x3Setosa[50];  p(x3|a)
-		x4Setosa[50];  p(x4|a)
-	
-		x1Versicolor[50];  p(x1|b)
-		x2Versicolor[50];  p(x2|b)
-		x3Versicolor[50];  p(x3|b)
-		x4Versicolor[50];  p(x3|b)
-	
-		x1Virginica[50];  p(x1|c)
-		x2Virginica[50];  p(x2|c)
-		x3Virginica[50];  p(x3|c)
-		x4Virginica[50];  p(x4|c)
-	*/
-	
-	/*
-	p(x|a) = p(x1|a)p(x2|a)p(x3|a)p(x4|a)
-	p(x|b) = p(x1|b)p(x2|b)p(x3|b)p(x4|b)
-	p(x|c) = p(x1|c)p(x2|c)p(x3|c)p(x4|c) 
-	*/
-	
+	float accuracyPercentage = ((float)correctCount / (float)totalCount) * 100;
+	printf("\nAccuracy: %i/%i, %.2f%%\n", correctCount, totalCount, accuracyPercentage);
+
 	return 0;
 }
-	
